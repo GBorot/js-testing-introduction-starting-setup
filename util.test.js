941c671f8048988f4ff121899a51bd444aa0fca1
Gautier Borot <g.borot@gmail.com>
2019-11-28 18:04:47 +0100

    end of Unit tests go to Integration tests

--- util.test.js
+++ util.test.js
@@ -4,9 +4,11 @@ const { generateText } = require("./util");
 test("should output name and age", () => {
   const text = generateText("Gautier", 28);
   expect(text).toBe("Gautier (28 years old)");
+  //   const text2 = generateText("Anna", 22);
+  //   expect(text2).toBe("Anna (22 years old)");
 });
 
-test("should output data-less text", () => {
-  const text = generateText("", null);
-  expect(text).toBe(" (null years old)");
-});
+// test("should output data-less text", () => {
+//   const text = generateText("", null);
+//   expect(text).toBe(" (null years old)");
+// });
