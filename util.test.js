ceeae85f73d7af13d2b7bae6a9ae39db0f0bf580
Gautier Borot <g.borot@gmail.com>
2019-11-28 17:43:44 +0100

    we wrote our first test

--- util.test.js
+++ util.test.js
@@ -1,1 +1,7 @@
 const { generateText } = require("./util");
+// import { generateText } from "./util"; // doesn't handle ES6 import in testing
+
+test("should output name and age", () => {
+  const text = generateText("Gautier", 28);
+  expect(text).toBe("Gautier (28 years old)");
+});
